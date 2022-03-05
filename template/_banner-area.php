54f31776fc96cd6e9929c1f1b0d8344f94d1b3af
Gian Carlo Gallano <59790388+ghiegie@users.noreply.github.com>
2022-03-05 08:05:58 +0800

    Add files via upload

---
+++ template/_banner-area.php
@@ -0,0 +1,14 @@
+        <!--owl carousel-->
+        <section id="banner-area">
+            <div class="owl-carousel owl-theme">
+                <div class="item">
+                    <img src="assets/ban1.png" alt="Banner 1">
+                </div>
+                <div class="item">
+                    <img src="assets/ban2.jpg" alt="Banner 2">
+                </div>
+                <div class="item">
+                    <img src="assets/ban1.png" alt="Banner 3">
+                </div>
+            </div>
+        </section>
